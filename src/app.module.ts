c4facd0373a345ca99b1608899c3dc757127ad01
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-01-14 22:52:10 +0900

    Add TypeORM and PostgreSQL dependencies and configure TypeORM connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ItemsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 15432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true, // エンティティの自動読み込み
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
