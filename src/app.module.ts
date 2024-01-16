b29cdc0fe262ebed1a1c68a24a0a8cbd49ac0f87
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-01-16 22:53:48 +0900

    Add Auth Directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
+import { AuthModule } from './auth/auth.module';
 
 const dbConfig = require("../ormconfig.js"); 
 @Module({
   imports: [
     ItemsModule,
-    TypeOrmModule.forRoot(dbConfig)
+    TypeOrmModule.forRoot(dbConfig),
+    AuthModule
   ]
 })
 export class AppModule {}
