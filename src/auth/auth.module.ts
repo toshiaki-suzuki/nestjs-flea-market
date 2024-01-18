5bb37bcdc831e51878251c70934750f801c7524e
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-01-18 17:20:37 +0900

    Add jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([UserRepository])],
+  imports:[
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),  // デフォルトの認証方法をJWTに設定
+    JwtModule.register({
+      secret: 'secret123',
+      signOptions: {
+        expiresIn: 3600,  // 1時間
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
